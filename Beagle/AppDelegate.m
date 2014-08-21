fd07256a325907f60ac5bce4f0f96c4adae4e9b7
Kanav Gupta <eulyssesx@gmail.com>
2014-08-21 14:01:43 -0400

    merge in changes

--- Beagle/AppDelegate.m
+++ Beagle/AppDelegate.m
@@ -663,7 +663,7 @@ void uncaughtExceptionHandler(NSException *exception) {
 
     // Handle errors
     if (error){
-        NSLog(@"errorode=%i",[FBErrorUtility errorCategoryForError:error]);
+        NSLog(@"errorode=%li",[FBErrorUtility errorCategoryForError:error]);
         NSLog(@"Error=%@",[error localizedDescription]);
         
         // If the error requires people using an app to make an action outside of the app in order to recover
